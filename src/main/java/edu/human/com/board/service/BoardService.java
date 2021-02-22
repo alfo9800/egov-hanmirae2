295a079c0bf7e8a6f216ee163993b0cbe3805032
user <user@DESKTOP-SI1MC9N>
2021-02-22 17:43:37 +0900

    admin board 구현중

--- src/main/java/edu/human/com/board/service/BoardService.java
+++ src/main/java/edu/human/com/board/service/BoardService.java
@@ -1,5 +1,5 @@
 package edu.human.com.board.service;
 
 public interface BoardService {
-	
+	public Integer delete_board(long nttId) throws Exception;
 }
--- src/main/java/edu/human/com/board/service/impl/BoardServiceImpl.java
+++ src/main/java/edu/human/com/board/service/impl/BoardServiceImpl.java
@@ -1,5 +1,22 @@
 package edu.human.com.board.service.impl;
 
-public class BoardServiceImpl {
+import javax.inject.Inject;
 
+import org.springframework.stereotype.Service;
+
+import edu.human.com.board.service.BoardService;
+
+@Service
+public class BoardServiceImpl implements BoardService {
+	
+	@Inject
+	private BoardDAO boardDAO; //같은 패키지 안에 있어서 import필요 없음
+
+	@Override
+	public Integer delete_board(long nttId) throws Exception {
+		//DAO호출
+		return boardDAO.delete_board(nttId);
+	}
+	
+	
 }
