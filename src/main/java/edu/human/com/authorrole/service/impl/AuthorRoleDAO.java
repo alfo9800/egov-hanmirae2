3cf5a28a8f92fcb56591502c8eb5f731dfa3843c
alfo9800 <alfo9800@naver.com>
2021-03-28 23:07:12 +0900

    authorrole dao -> service -> ser.impl

--- src/main/java/edu/human/com/authorrole/service/impl/AuthorRoleDAO.java
+++ src/main/java/edu/human/com/authorrole/service/impl/AuthorRoleDAO.java
@@ -1,7 +1,18 @@
 package edu.human.com.authorrole.service.impl;
 
-import edu.human.com.common.EgovComAbstractMapper;
+import java.util.List;
 
+import org.springframework.stereotype.Repository;
+
+import edu.human.com.authorrole.service.AuthorRoleVO;
+import edu.human.com.common.EgovComAbstractMapper;
+import edu.human.com.util.PageVO;
+
+@Repository
 public class AuthorRoleDAO extends EgovComAbstractMapper {
-	
+	public List<AuthorRoleVO> selectAuthorRole(PageVO pageVO) throws Exception {
+		//pageVO는 초기값이 필요. 초기값은 Controller클래스에 입력하게 됨.
+		List<AuthorRoleVO> authorRoleList = selectList("authorroleMapper.selectAuthorRole", pageVO);
+		return authorRoleList;
+	}
 }
